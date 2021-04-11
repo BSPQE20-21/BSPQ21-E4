2f8981440545e90bebe190cc8a2910f77bb73c91
Gabriel <gabriel.gdebaquedano@opendeusto.es>
2021-04-11 13:38:26 +0200

    Guest user done and premium user corrected

--- src/main/java/bspq21_e4/ParkingManagement/server/data/GuestUser.java
+++ src/main/java/bspq21_e4/ParkingManagement/server/data/GuestUser.java
@@ -1,16 +1,52 @@
 package bspq21_e4.ParkingManagement.server.data;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class GuestUser extends User {
 
 	String plate;
+	Date entranceDate;
+	Slot selectedSlot;
 	
+	public static SimpleDateFormat sdfResult = new SimpleDateFormat("HH:mm", Locale.US);
 	
-	
-	public GuestUser(String plate) {
-		
-		
+	public GuestUser(String plate, Date entranceDate, Slot selectedSlot) {
+		super();
+		this.plate = plate;
+		this.entranceDate = entranceDate;
+		this.selectedSlot = selectedSlot;
 	}
 
-	
+	public String getPlate() {
+		return plate;
+	}
+
+	public void setPlate(String plate) {
+		this.plate = plate;
+	}
+
+	public Date getEntranceDate() {
+		return entranceDate;
+	}
+
+	public void setEntranceDate(Date entranceDate) {
+		this.entranceDate = entranceDate;
+	}
+
+	public Slot getSelectedSlot() {
+		return selectedSlot;
+	}
+
+	public void setSelectedSlot(Slot selectedSlot) {
+		this.selectedSlot = selectedSlot;
+	}
+
+	@Override
+	public String toString() {
+		return "GuestUser [plate=" + plate + ", entranceDate=" + entranceDate + ", selectedSlot=" + selectedSlot + "]";
+	}
+
 	
 }
--- src/main/java/bspq21_e4/ParkingManagement/server/data/PremiumUser.java
+++ src/main/java/bspq21_e4/ParkingManagement/server/data/PremiumUser.java
@@ -5,8 +5,10 @@ public class PremiumUser extends User{
 	String email;
 	String plate;
 	int monthfee;
+	Slot selectedSlot;
 
-	public PremiumUser(String email, String plate, int monthfee) {
+	public PremiumUser(String email, String plate, int monthfee, Slot selectedSlot) {
+		super();
 		this.email = email;
 		this.plate = plate;
 		this.monthfee = monthfee;
@@ -36,9 +38,18 @@ public class PremiumUser extends User{
 		this.monthfee = monthfee;
 	}
 
+	public Slot getSelectedSlot() {
+		return selectedSlot;
+	}
+
+	public void setSelectedSlot(Slot newSelectedSlot) {
+		this.selectedSlot = newSelectedSlot;
+	}
+
 	@Override
 	public String toString() {
-		return "PremiumUser [, email=" + email + ", plate=" + plate + ", monthfee=" + monthfee + "]";
+		return "PremiumUser [email=" + email + ", plate=" + plate + ", monthfee=" + monthfee + ", selectedSlot="
+				+ selectedSlot + "]";
 	}
 
 }
