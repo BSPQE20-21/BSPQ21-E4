bd33194637afd356cd73bc034437d9ddb1aa3edc
jonmaeztu <47626278+jonmaeztu@users.noreply.github.com>
2021-04-24 11:29:45 +0200

    Service Locator removed

--- src/main/java/bspq21_e4/ParkingManagement/server/remote/IRemoteFacade.java
+++ src/main/java/bspq21_e4/ParkingManagement/server/remote/IRemoteFacade.java
@@ -1,11 +1,17 @@
 package bspq21_e4.ParkingManagement.server.remote;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 
 
 
 public interface IRemoteFacade extends Remote{
+	
+
 	
 
 
