ffbe0c0526e78473d92a76ed596a47cc84dfc599
Jon <jon.churruca@opendeusto.es>
2021-04-29 08:22:59 +0200

    Corrección del error de falta de test en el cmd, nueva clase de test para la BD

---
+++ src/test/java/bspq21_e4/ParkingManagement/server/data/test/apiTest.java
@@ -0,0 +1,14 @@
+package bspq21_e4.ParkingManagement.server.data.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class apiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
