15c362abf69063355f9cff2c3d25f66f8ee83338
jonmaeztu <47626278+jonmaeztu@users.noreply.github.com>
2021-05-24 15:26:41 +0200

    Final

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -2,6 +2,6 @@
 /* CREATE ''parkingmanagement' DATABASE */
 CREATE SCHEMA parkingmanagement;
 /* CREATE THE USER 'root' AT LOCAL SERVER WITH PASSWORD '1234' */
-CREATE USER 'root'@'%' IDENTIFIED BY '1234';
+CREATE USER  if not exists 'root'@'%' IDENTIFIED BY '1234';
 /* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'root' AT LOCAL SERVER*/
 GRANT ALL ON parkingmanagement.* TO 'root'@'%';
